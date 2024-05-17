91d6929a9339b2d1afcd351f0e144f1da994f8aa
Luis <alberto_1102@hotmail.com>
2024-05-17 17:11:49 -0500

    con gmail login v2

--- app/Controllers/Inicio.php
+++ app/Controllers/Inicio.php
@@ -319,7 +319,37 @@ class Inicio extends BaseController
 
     private function _loginGmail(){
 
-        //
+        $clientID = '';
+        $clientSecret = '';
+        $redirectUri = '';
+
+        // create Client Request to access Google API
+        $client = new \Google_Client();
+        $client->setClientId($clientID);
+        $client->setClientSecret($clientSecret);
+        $client->setRedirectUri($redirectUri);
+        $client->addScope("email");
+        $client->addScope("profile");
+
+        // authenticate code from Google OAuth Flow
+        if (isset($_GET['code']) && $_GET['code'] ) {
+            $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+            $client->setAccessToken($token['access_token']);
+
+            $google_oauth = new \Google\Service\Oauth2($client);
+            $google_account_info = $google_oauth->userinfo->get();
+            $email =  $google_account_info->email;
+            $name =  $google_account_info->name;
+            
+            /* echo "$email - $name";
+            echo "<pre>";
+            print_r($google_account_info);
+            echo "</pre>"; */
+            return array( 'tipo' => 'datosGoogle', 'datos' => [$email, $name] );
+        } else {
+            //echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+            return array( 'tipo' => 'linkGoogle', 'link' => $client->createAuthUrl() );
+        }
     }
 
     private function loginConGoogle($arr){
